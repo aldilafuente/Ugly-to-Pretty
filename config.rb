a0fe736cb262b7a46ee12a469491ed1383c170b4
Aldana Lafuente <44821947+aldilafuente@users.noreply.github.com>
2019-01-11 14:41:42 -0300

    Almost there

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 
 css_dir = "css" # by Compass.app 
 sass_dir = "sass" # by Compass.app 
-images_dir = "images" # by Compass.app 
+images_dir = "img" # by Compass.app 
 output_style = :expanded # by Compass.app 
 relative_assets = false # by Compass.app 
 line_comments = true # by Compass.app 
